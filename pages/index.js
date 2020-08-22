096bb784bc1197f5a088871e640cbf1bc5cf43e8
Kripa Rajshekhar <kripa@metolabs.com>
2020-08-22 10:40:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,60 @@
-import styles from '../styles/Home.module.css'
+iimport styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-       <div className="container">
-      <Head>
-        <title>Fat Neo -- Crash Cover</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          <a href="https://fatneo.com"> Fat Neo -- Crash Cover</a>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className="description">
-          A defensive backstop to invest full on!
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className="grid">
-          <a href="https://fatneo.com" className="card">
-          <h3>SPY Oct. 16 &darr; 5%</h3>
-            <p>3 Cents per $1 Cover</p>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://fatneo.com" className="card">
-          <h3>SPY Oct. 16 &darr; 10%</h3>
-            <p>0.3 Cents per $1 Cover</p>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://whatstrading.com"
-            className="card"
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
           >
-            <h3>Here's the data &rarr;</h3>
-            <p>A firehose of goodness.</p>
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
+            className={styles.card}
           >
-            <h3>The fundamentals &rarr;</h3>
+            <h3>Deploy &rarr;</h3>
             <p>
-             Everyone needs options.
+              Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
   )
 }
+
