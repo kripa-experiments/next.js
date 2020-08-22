8c68dabb79cd051480213d893c8488264b99abdb
Kripa Rajshekhar <kripa@metolabs.com>
2020-08-22 13:28:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,15 +46,18 @@ export default function Home() {
           </a>
         </div>
     
-        <p className={styles.description}
-           style="color:blue;">Is this like insurance for stocks?
-        </p>
+        <h2 className={styles.description}>
+            Is this like insurance for stocks?
+        </h2>
         <p className={styles.description}>
            Yup. SPY losses limited to &#x1F53B; % crash-covered.
         </p>
         <p className={styles.description}>
-           &#x274C; &#x1F61F; = &#x1F60F; &#10548;. Fat Neo style.
+           &#x274C; &#x1F61F; = &#x1F60F; &#10548;
         </p>
+         <h3 className={styles.description}>
+          <a href="https://fatneo.com">Fat Neo style.</a>
+          </h3>
       </main>
     </div>
   )
